23796c07b0f5a41fe05a5b064dc77fd9cee51273
ashish4hub <ashish.k620045@gmail.com>
2026-03-11 17:19:03 +0530

    Implement RX ring buffer for UART data reception
    Refactor RX code to implement a ring buffer for UART reception and improve data handling.

--- Rx_code.c
+++ Rx_code.c
@@ -1,44 +1,91 @@
-// RX code 
-// upload on Receiver IC
+// RX ring buffer code 
+// Upload on RX IC
 
-
-#define F_CPU 16000000
+#define F_CPU 16000000UL
 #include<avr/io.h>
 #include<avr/interrupt.h>
+#include<string.h>
 #define BAUD 9600
 #define BRC ((F_CPU / (16 * BAUD)) - 1)
-volatile char rx_data;
-void uart_rx_ini(){
+#define rx_buff_size 64
+volatile char rx_buff[rx_buff_size];
+volatile uint8_t rx_head = 0;
+volatile uint8_t rx_tail = 0;
+
+//Configure UART RX
+void rx_ini(void){
+  UCSR0B = (1 << RXCIE0) | (1 << RXEN0);
   UBRR0H = (BRC >> 8);
   UBRR0L = BRC;
-  UCSR0B = (1 << RXCIE0) | (1 << RXEN0);
-  UCSR0C = ( 1 << UCSZ01) | (1 << UCSZ00);
-  sei();
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
+//Interrupt function only read the byte received and store in 'data'
 ISR(USART_RX_vect){
-  rx_data = UDR0; // ISR writes the received byte
+  char data = UDR0; // Read received byte in data 
+  uint8_t next = (rx_head + 1) % rx_buff_size;
+  
+  //Drop byte is buffer is full
+  if(next != rx_tail){
+    rx_buff[rx_head] = data;
+    rx_head = next;
+  }
 }
-int main (void){
-  uart_rx_ini();
-  DDRB |= (1 << PB5);
-  DDRB |= (1 << PB4);
-  DDRB |= (1 << PB3);
+uint8_t uart_rx_avail(void){
+  return (rx_head != rx_tail);
+}
+char uart_get_data(void){
+  if(rx_head == rx_tail) return 0;
+  char data = rx_buff[rx_tail];
+  rx_tail = (rx_tail + 1) % rx_buff_size;
+  return data;
+}
+//Building line 
+#define line_size 32
+char line[line_size];
+uint8_t line_idx = 0;
+uint8_t read_line(void){
+  while(uart_rx_avail()){
+    char c = uart_get_data();
+    if(c == '\n' || c == '\r'){
+      line[line_idx] = '\0'; //end string
+      line_idx = 0;
+      return 1;
+    }
+    if(line_idx < line_size - 1){
+      line[line_idx++] = c;
+    }
+  }
+  return 0;
+}
+int main(void){
+  rx_ini();
+  sei();
+  DDRB |= (1 << PB5); // OUTPUT
+  DDRB |= (1 << PB4); // OUTPUT
+  DDRB |= (1 << PB3); // OUTPUT
+
   while(1){
-    if(rx_data == 'B'){
-      PORTB |= (1 << PB4);
-      PORTB &= ~(1 << PB5);
-      PORTB &= ~(1 << PB3);
-    }
-    else if(rx_data == 'R'){
-      PORTB |= (1 << PB5);
-      PORTB &= ~(1 << PB4);
-      PORTB &= ~(1 << PB3);
-    }
-    else if(rx_data == 'O'){
-      PORTB |= (1 << PB3);
-      PORTB &= ~(1 << PB5);
-      PORTB &= ~(1 << PB4);
-    
+    if(read_line()){
+      
+      if(strcmp(line,"RED LED ON") == 0){
+        PORTB |= (1 << PB5);
+        PORTB &= ~(1 << PB4);
+        PORTB &= ~(1 << PB3);
+
+      }
+       
+      else if(strcmp(line,"BLUE LED ON") == 0){
+              PORTB |= (1 << PB4);
+              PORTB &= ~(1 << PB5);
+              PORTB &= ~(1 << PB3);
+
+      }
+      else if(strcmp(line, "ORANGE LED ON") == 0){
+              PORTB |= (1 << PB3);
+              PORTB &= ~(1 << PB5);
+              PORTB &= ~(1 << PB4);
+
+      }
     }
   }
 }
